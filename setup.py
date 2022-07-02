b3aa7e451d16339fdf3df1ec8498e0b3f97eac3b
ipriyam26 <ipriyam26@gmail.com>
2022-07-02 22:57:04 +0530

    version 0.3.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name="influx_line_mqtt",
-    version="0.2.0",
+    version="0.3.0",
     description="A library to publish data to InfluxDB using MQTT",
     long_description=open("readme.txt").read(),
     author="Priyam Srivastava",
