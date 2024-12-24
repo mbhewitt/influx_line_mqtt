16ecda7891dc52a1605a7bfa138d75d5cdd0b788
mbhewitt <dircery@gmail.com>
2024-12-24 13:21:14 -0800

    0.3.5 test

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 setup(
     name="influx_line_mqtt",
-    version="0.3.3",
+    version="0.3.5",
     description="A library to publish data to InfluxDB using MQTT",
     long_description=open("README.md").read(),
     author="Mew Hewitt",
     packages=["influx_line_mqtt"],
-    install_requires=["paho-mqtt","influx-line-protocol"],
+    install_requires=["paho-mqtt<2.0",
+'influx_line_protocol @ git+https://github.com/mbhewitt/influx_line_protocol#egg=/influx_line_protocol'],
 )
